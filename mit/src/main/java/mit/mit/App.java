d514a137040111efee9ff5da2d4cf08e0b0ff2fc
nimit <jhaallen14@gmail.com>
2020-05-20 18:12:35 +0530

    repository from ecilipse

---
+++ mit/src/main/java/mit/mit/App.java
@@ -0,0 +1,13 @@
+package mit.mit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
